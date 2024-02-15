3db6f709a7aeb6e4e44570dc6bf95cbcb9e8039f
Jakub Piksa <kuba.piksa@wp.pl>
2024-02-15 13:40:40 +0100

    iucons fix

--- src/shared/Footer/footer.tsx
+++ src/shared/Footer/footer.tsx
@@ -1,12 +1,5 @@
 import { FC } from "react";
 import logoImage from "../../assets/Znak_wodny_black.png";
-import {
-  EnvelopeIcon,
-  FacebookIcon,
-  InstagramIcon,
-  PhoneIcon,
-  LocationIcon,
-} from "../../icons/icons";
 import "./footer.scss";
 import { NavLink } from "react-router-dom";
 
@@ -19,26 +12,26 @@ export const Footer: FC = () => {
       <div className="footer-content">
         <div className="contact">
           <a href="tel:+48508282405">
-            508 282 405 <img src={PhoneIcon} alt="phone" />
+            508 282 405 <img src="../../icons/phone.svg" alt="phone" />
           </a>
           <a href="https://maps.app.goo.gl/UxuRVt75BcmVcosy8">
             ul. Osińska 64/4 44-240 Żory
-            <img src={LocationIcon} alt="location" />
+            <img src="../../icons/location.svg" alt="location" />
           </a>
         </div>
         <div className="socials">
           <a href="https://www.facebook.com/profile.php?id=100067095032900">
-            FB <img src={FacebookIcon} alt="fb"></img>
+            FB <img src="../../icons/facebook.svg" alt="fb"></img>
           </a>
           <a href="https://www.instagram.com/_bunny_art_studio?utm_source=ig_web_button_share_sheet&igsh=ZDNlZDc0MzIxNw==">
-            IG <img src={InstagramIcon} alt="ig"></img>
+            IG <img src="../../icons/instagram.svg" alt="ig"></img>
           </a>
           <a href="mailto:bunny.art.mk@gmail.com">
-            bunny.art.mk@gmail.com <img src={EnvelopeIcon} alt="email" />
+            bunny.art.mk@gmail.com{" "}
+            <img src="../../icons/envelope.svg" alt="email" />
           </a>
         </div>
       </div>
-      <p>Footer</p>
     </div>
   );
 };
