66a747eb6d50a516e7a3f1c07c7513901d5cf5d4
Jakub Piksa <kuba.piksa@wp.pl>
2024-02-19 17:42:41 +0100

    Header & footer styling finish

--- src/shared/components/MobileMenu/mobilemenu.tsx
+++ src/shared/components/MobileMenu/mobilemenu.tsx
@@ -1,6 +1,7 @@
 import { FC, useState } from "react";
 import { NavLink } from "react-router-dom";
 import "./mobilemenu.scss";
+import closeIcon from "../../../icons/close.svg";
 
 export const MobileMenu: FC = () => {
   const [isOpen, setIsOpen] = useState(false);
@@ -22,8 +23,11 @@ export const MobileMenu: FC = () => {
       ) : null}
 
       {isOpen ? (
-        <div className={`mobile-menu ${isOpen ? "open" : ""}`}>
-          <nav className="nav">
+        <div className={`mobile-menu ${isOpen && "open"}`}>
+          <button className="close-icon" onClick={handleMenuClose}>
+            <img src={closeIcon} alt="close"></img>
+          </button>
+          <nav className="mobile-nav">
             <NavLink to={"/home"}>O nas</NavLink>
             <NavLink to={"/home"}>Galeria</NavLink>
             <NavLink to={"/home"}>Usługi</NavLink>
