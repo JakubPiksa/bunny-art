4c5e5af9167d910b4e907be879edb8a9935d3132
Jakub Piksa <kuba.piksa@wp.pl>
2024-03-07 22:44:25 +0100

    Update contact.tsx

--- src/shared/components/Contact/contact.tsx
+++ src/shared/components/Contact/contact.tsx
@@ -3,5 +3,5 @@ import { FC } from "react";
 import "./contact.scss";
 
 export const Contact: FC = () => {
-  return <div className="gallery">KONTAJT</div>;
+  return <div className="gallery">Kontact</div>;
 };
