373f57851bc704235d2474084686322a96628b77
Jakub Piksa <kuba.piksa@wp.pl>
2024-03-19 17:02:44 +0100

    p[rices

--- src/shared/components/PriceList/pricelist.tsx
+++ src/shared/components/PriceList/pricelist.tsx
@@ -4,8 +4,35 @@ import "./pricelist.scss";
 
 export const PriceList: FC = () => {
   return (
-    <div>
+    <div className="prices">
       <h1>Cennik</h1>
+      <div className="prices-container">
+        <div className="prices-container-element">
+          <div className="prices-container-element-title">
+            <span></span>
+            <h2>maincure</h2>
+            <span></span>
+          </div>
+          <div className="prices-container-element-name">
+            <p>Maincure hybrydowt</p> <p>90zł</p>
+          </div>
+          <div className="prices-container-element-name">
+            <p>Maincure hybrydowt</p> <p>90zł</p>
+          </div>
+          <div className="prices-container-element-name">
+            <p>Maincure hybrydowt</p> <p>90zł</p>
+          </div>
+          <div className="prices-container-element-name">
+            <p>Maincure hybrydowt</p> <p>90zł</p>
+          </div>
+          <div className="prices-container-element-name">
+            <p>Maincure hybrydowt</p> <p>90zł</p>
+          </div>
+          <div className="prices-container-element-name">
+            <p>Maincure hybrydowt</p> <p>90zł</p>
+          </div>
+        </div>
+      </div>
     </div>
   );
 };
