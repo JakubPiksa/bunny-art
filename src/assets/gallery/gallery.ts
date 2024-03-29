12d19132d3ac0c7902e95d977a7b4cd48fd5cfd8
Jakub Piksa <kuba.piksa@wp.pl>
2024-03-29 17:28:38 +0100

    galeria

---
+++ src/assets/gallery/gallery.ts
@@ -0,0 +1,93 @@
+import photo1 from "../../assets/gallery/charakteryzation/IMG-20221023-WA0012.jpg";
+import photo2 from "../../assets/gallery/charakteryzation/IMG-20221023-WA0013.jpg";
+import photo3 from "../../assets/gallery/charakteryzation/IMG-20221023-WA0014.jpg";
+import photo4 from "../../assets/gallery/charakteryzation/Screenshot_20230914_100316_Gallery.jpg";
+import photo5 from "../../assets/gallery/charakteryzation/Screenshot_20230914_100327_Gallery.jpg";
+import photo6 from "../../assets/gallery/charakteryzation/Screenshot_20230914_100336_Gallery.jpg";
+import photo7 from "../../assets/gallery/charakteryzation/Screenshot_20230914_100350_Gallery.jpg";
+import photo8 from "../../assets/gallery/charakteryzation/Screenshot_20230914_100421_Gallery.jpg";
+import photo9 from "../../assets/gallery/charakteryzation/Screenshot_20230914_100529_Gallery.jpg";
+import photo10 from "../../assets/gallery/charakteryzation/Screenshot_20230914_100609_Gallery.jpg";
+import photo11 from "../../assets/gallery/charakteryzation/Screenshot_20230914_100633_Gallery.jpg";
+import photo12 from "../../assets/gallery/charakteryzation/Screenshot_20230914_100702_Gallery.jpg";
+import photo13 from "../../assets/gallery/charakteryzation/Screenshot_20230914_100712_Gallery.jpg";
+import photo14 from "../../assets/gallery/charakteryzation/Screenshot_20230914_101054_Gallery.jpg";
+import photo15 from "../../assets/gallery/charakteryzation/Screenshot_20230914_101108_Gallery.jpg";
+import photo16 from "../../assets/gallery/charakteryzation/Screenshot_20230914_101128_Gallery.jpg";
+import photo17 from "../../assets/gallery/charakteryzation/Screenshot_20230914_101154_Gallery.jpg";
+import photo18 from "../../assets/gallery/charakteryzation/Screenshot_20230914_101201_Gallery.jpg";
+import photo19 from "../../assets/gallery/charakteryzation/Screenshot_20230914_101206_Gallery.jpg";
+import photo20 from "../../assets/gallery/charakteryzation/Screenshot_20230914_101211_Gallery.jpg";
+import photo21 from "../../assets/gallery/nails/20240119_095607-wm-141108.59999999776_watermarked.jpg";
+import photo22 from "../../assets/gallery/nails/IMG_20231209_180309_550.jpg";
+import photo23 from "../../assets/gallery/nails/MTXX_MH20230322_102550460.jpg";
+import photo24 from "../../assets/gallery/nails/MTXX_MH20230915_142510918.jpg";
+import photo25 from "../../assets/gallery/nails/MTXX_MH20230915_142905972.jpg";
+import photo26 from "../../assets/gallery/nails/MTXX_MH20230915_143029046.jpg";
+import photo27 from "../../assets/gallery/nails/MTXX_MR20230724_142919992.jpg";
+import photo28 from "../../assets/gallery/nails/MTXX_MR20230731_185901833.jpg";
+import photo29 from "../../assets/gallery/nails/Screenshot_20230914_100729_Gallery.jpg";
+import photo30 from "../../assets/gallery/nails/Screenshot_20230914_100954_Gallery.jpg";
+import photo31 from "../../assets/gallery/nails/Screenshot_20230914_101143_Gallery.jpg";
+import photo32 from "../../assets/gallery/nails/Screenshot_20230914_101429_Gallery.jpg";
+import photo33 from "../../assets/gallery/nails/Screenshot_20230914_101714_Gallery.jpg";
+import photo34 from "../../assets/gallery/nails/Screenshot_20230914_101836_Gallery.jpg";
+import photo35 from "../../assets/gallery/nails/Screenshot_20230914_102131_Gallery.jpg";
+import photo36 from "../../assets/gallery/nails/Screenshot_20230914_102159_Gallery.jpg";
+import photo37 from "../../assets/gallery/salon/IMG_1258.jpeg";
+import photo38 from "../../assets/gallery/salon/IMG_1263.jpeg";
+import photo39 from "../../assets/gallery/salon/IMG_1373.jpeg";
+import photo40 from "../../assets/gallery/salon/IMG_1375.jpeg";
+import photo41 from "../../assets/gallery/salon/IMG_1381.jpeg";
+import photo42 from "../../assets/gallery/salon/IMG_1384.jpeg";
+import photo43 from "../../assets/gallery/salon/IMG_1398.jpeg";
+import photo44 from "../../assets/gallery/salon/IMG_1399.jpeg";
+
+export const photoImages = [
+  photo1,
+  photo2,
+  photo3,
+  photo4,
+  photo5,
+  photo6,
+  photo7,
+  photo8,
+  photo9,
+  photo10,
+  photo11,
+  photo12,
+  photo13,
+  photo14,
+  photo15,
+  photo16,
+  photo17,
+  photo18,
+  photo19,
+  photo20,
+
+  photo21,
+  photo22,
+  photo23,
+  photo24,
+  photo25,
+  photo26,
+  photo27,
+  photo28,
+  photo29,
+  photo30,
+  photo31,
+  photo32,
+  photo33,
+  photo34,
+  photo35,
+  photo36,
+
+  photo37,
+  photo38,
+  photo39,
+  photo40,
+  photo41,
+  photo42,
+  photo43,
+  photo44,
+];
--- src/shared/components/Gallery/gallery.tsx
+++ src/shared/components/Gallery/gallery.tsx
@@ -2,37 +2,22 @@ import { FC } from "react";
 
 import "./gallery.scss";
 
-import { salonImages } from "../../../assets/gallery/salon/salon";
-import { nailsImages } from "../../../assets/gallery/nails/nails";
-import { charakteryzationImages } from "../../../assets/gallery/charakteryzation/charakteryzation";
+import { photoImages } from "../../../assets/gallery/gallery";
 
 export const Gallery: FC = () => {
   return (
     <div className="gallery">
       <h2>Galeria</h2>
+
       <div className="gallery-part">
-        <h3>Salon</h3>
-        <div className="gallery-part-image">
-          {salonImages.map((photo, index) => (
-            <img key={index} src={photo} alt={`Salon ${index + 1}`} />
-          ))}
-        </div>
-      </div>
-      <div className="gallery-part">
-        <h3>Charakteryzacja</h3>
-        <div className="gallery-part-image">
-          {charakteryzationImages.map((photo, index) => (
-            <img key={index} src={photo} alt={`Charakteryzacja ${index + 1}`} />
-          ))}
-        </div>
-      </div>
-      <div className="gallery-part">
-        <h3>Nails</h3>
-        <div className="gallery-part-image">
-          {nailsImages.map((photo, index) => (
-            <img key={index} src={photo} alt={`Nails ${index + 1}`} />
-          ))}
-        </div>
+        {photoImages.map((photo, index) => (
+          <img
+            key={index}
+            src={photo}
+            alt={`Nails ${index + 1}`}
+            className="gallery-part-image"
+          />
+        ))}
       </div>
     </div>
   );
