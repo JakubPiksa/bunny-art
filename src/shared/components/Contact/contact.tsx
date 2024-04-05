48588babd85d61cb2f5bc473f72db5db313c0338
Jakub Piksa <kuba.piksa@wp.pl>
2024-04-05 16:01:40 +0200

    map finish

--- src/shared/components/Contact/contact.tsx
+++ src/shared/components/Contact/contact.tsx
@@ -63,11 +63,12 @@ export const Contact: FC = () => {
         </a>
       </div>
       <div className="contactS-map">
+        <h2>Znajdziesz nas tutaj!</h2>
         <iframe
           src="https://www.google.com/maps/embed?pb=!1m17!1m12!1m3!1d2562.8763363336375!2d18.693963999999998!3d50.032413!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m2!1m1!2zNTDCsDAxJzU2LjciTiAxOMKwNDEnMzguMyJF!5e0!3m2!1spl!2spl!4v1712322238874!5m2!1spl!2spl"
           width="100%"
           height="450"
-          style={{ border: "1px solid black" }}
+          style={{ border: "1px solid #86868620" }}
           allowFullScreen={true}
           loading="lazy"
           referrerPolicy="no-referrer-when-downgrade"
