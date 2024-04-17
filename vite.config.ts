3213bb768a4cbb2231e105f51f47fb5aac6af7a7
Jakub Piksa <kuba.piksa@wp.pl>
2024-04-17 17:07:08 +0200

    fix

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,9 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
-import VitePluginSass from "vite-plugin-sass";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react(), VitePluginSass()],
+  plugins: [react()],
   server: {
     port: 3000,
   },
