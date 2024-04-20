2744fc11eedae67373c6239beff6825e57d0722e
Jakub Piksa <117599249+JakubPiksa@users.noreply.github.com>
2024-04-20 11:44:00 +0200

    Update sitenotfound.tsx

--- src/shared/components/SiteNotFound/sitenotfound.tsx
+++ src/shared/components/SiteNotFound/sitenotfound.tsx
@@ -7,13 +7,10 @@ export const SiteNotFound: FC = () => {
   return (
     <div>
       <h3 className="error">
-        I apologize for any confusion. It appears that I may have lost my way in
-        the vast expanse of the internet. Allow me to return you to the world of
-        movies, where we can continue our journey together. Please click the
-        button below to return to the planet of movies.
+        Ta ścieżka nie istnieje.
       </h3>
       <Link to={`/home`}>
-        <button>Go Back</button>
+        <button>Strona główna</button>
       </Link>
     </div>
   );
